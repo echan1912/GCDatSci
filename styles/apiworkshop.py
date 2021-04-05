0eb15e95dcc3776b225b071358366ddfe25933a4
echan1912 <echan1912@gmail.com>
2021-04-05 20:34:48 +0800

    spotify python

--- styles/apiworkshop.py
+++ styles/apiworkshop.py
@@ -9,9 +9,13 @@ def get_config():
     return[c['api_key']]
 
 # writing GET request
-    spotify = requests.get("https://api.spotify.com/v1/search?q=Mamoru%20Miyano&type=artist", header={'api_key': api_key},
+def get_spotify(api_key):
+    url = requests.get("https://api.spotify.com/v1/search?q=Mamoru%20Miyano&type=artist", header={'api_key': api_key},
     params={'q': 'Mamoru Miyano', 'type':'artist'}
+    r = requests.get(url)
+    return r.json()
     
+
 def main():
 	config = get_config()
 	api_key= config[0]
