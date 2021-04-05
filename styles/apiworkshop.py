3e0dbb4dc4f7e9e7cdb13fe2a4259af336204049
echan1912 <echan1912@gmail.com>
2021-04-05 19:51:37 +0800

    added spotify url

--- styles/apiworkshop.py
+++ styles/apiworkshop.py
@@ -5,8 +5,18 @@ import pprint from pprint #prints json nicely
 def get_config():
     c = configparser.ConfigParser()
     c.read('config.ini') #reads name of file
-    return 
+    return[c['spotify']['api_key']]
+
+# writing GET request
+def get_spotify():
+    url = "https://api.spotify.com/v1/search?q=Mamoru%20Miyano&type=artist&access_token=\()".format(api_key)
+    r = requests.get(url)
+    return r.json()
 
 def main():
-    config = get_config()
-    api_key, 
+    config = get_config() 
+    api_key = config[0]
+    spotify = get_spotify(api_key)
+
+    pprint(spotify)
+
