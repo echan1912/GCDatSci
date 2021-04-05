8de732890412689895fbf1ca161f51c99bb858e3
echan1912 <echan1912@gmail.com>
2021-04-05 20:34:19 +0800

    i tried to make a spotify api with python

--- styles/apiworkshop.py
+++ styles/apiworkshop.py
@@ -1,21 +1,24 @@
 import configparser
 import requests
+import sys
 
+#retrieves api key
 def get_config():
     c = configparser.ConfigParser()
     c.read('config.ini') #reads name of file
-    return[c['spotify']['api_key']]
+    return[c['api_key']]
 
 # writing GET request
-def get_spotify():
-    url = requests.get("https://api.spotify.com/v1/search?q=Mamoru%20Miyano&type=artist", header={'api_key'})
-    r = requests.get(url)
-    return r.json()
-
+    spotify = requests.get("https://api.spotify.com/v1/search?q=Mamoru%20Miyano&type=artist", header={'api_key': api_key},
+    params={'q': 'Mamoru Miyano', 'type':'artist'}
+    
 def main():
-    config = get_config() 
-    api_key = config[0]
-    spotify = get_spotify(api_key)
-    print(spotify)
+	config = get_config()
+	api_key= config[0]
+	spotify = get_spotify(api_key)
 
-main()
+	print(spotify)
+    print("{: .if} is the best artist.").format('type')
+
+if __name__ == '__main__':
+	main()
